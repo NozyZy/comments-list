ad68226b7baa36abf90294303c34d3502e427ac9
Restyled.io <commits@restyled.io>
2021-12-14 15:47:22 +0000

    Restyled by prettier

--- src/assets/datas.js
+++ src/assets/datas.js
@@ -1,117 +1,131 @@
 import axios from "axios";
-import VueJwtDecode from 'vue-jwt-decode';
+import VueJwtDecode from "vue-jwt-decode";
 
-const url = 'http://192.168.84.250:5000'
+const url = "http://192.168.84.250:5000";
 
 export default {
-
-  api: url + '/api/comments', auth: url + '/auth', loggedin: false, options: {
-    method: 'get',
+  api: url + "/api/comments",
+  auth: url + "/auth",
+  loggedin: false,
+  options: {
+    method: "get",
     headers: new Headers({
-      'content-type' : 'application/json',
-      'Authorization' : localStorage.getItem("user")
+      "content-type": "application/json",
+      Authorization: localStorage.getItem("user"),
     }),
   },
 
-      async getAll() {
+  async getAll() {
     return fetch(this.api, this.options)
-        .then(data => data.json())
-        .then(json => {return json});
-  }
-  , async find(name) {
-    return fetch(this.api + '/find?name=' + name, this.options)
-        .then(data => data.json())
-        .then(json => {return json});
-  }
-  , create(data) {
+      .then((data) => data.json())
+      .then((json) => {
+        return json;
+      });
+  },
+  async find(name) {
+    return fetch(this.api + "/find?name=" + name, this.options)
+      .then((data) => data.json())
+      .then((json) => {
+        return json;
+      });
+  },
+  create(data) {
     axios({
-      method : 'post',
-      url : this.api + '/create',
-      headers : {
-        'Content-Type' : 'application/json',
-        'Authorization' : localStorage.getItem("user"),
+      method: "post",
+      url: this.api + "/create",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("user"),
       },
-      data : data
+      data: data,
     });
-  }
-  , toggleHidden(id, hidden) {
+  },
+  toggleHidden(id, hidden) {
     axios({
-      method : 'post',
-      url : this.api + '/update?id=' + id + '&hidden=' + hidden,
-      headers : {
-        'Content-Type' : 'application/json',
-        'Authorization' : localStorage.getItem("user"),
-      },
-    });
-  }
-  , delete (id) {
-    axios({
-      method : 'delete',
-      url : this.api + '/delete?id=' + id,
-      headers : {
-        'Content-Type' : 'application/json',
-        'Authorization' : localStorage.getItem("user"),
+      method: "post",
+      url: this.api + "/update?id=" + id + "&hidden=" + hidden,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("user"),
       },
     });
-  }
-  , register(newUser) {
+  },
+  delete(id) {
     axios({
-      method : 'post',
-      url : this.auth + '/register',
-      headers : {
-        'Content-Type' : 'application/json',
+      method: "delete",
+      url: this.api + "/delete?id=" + id,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("user"),
       },
-      data : newUser
     });
-  }
-  , login(creds) {
+  },
+  register(newUser) {
+    axios({
+      method: "post",
+      url: this.auth + "/register",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: newUser,
+    });
+  },
+  login(creds) {
     const res = axios({
-      method : 'post',
-      url : this.auth + '/login',
-      headers : {
-        'Content-Type' : 'application/json',
+      method: "post",
+      url: this.auth + "/login",
+      headers: {
+        "Content-Type": "application/json",
       },
-      data : creds
+      data: creds,
     });
-    res.then(res => {
-         if (res.status === 200) {
-           let token = res.data.token;
-           localStorage.setItem("user", token);
-         }
-       })
-        .catch(err => { console.error(err); });
+    res
+      .then((res) => {
+        if (res.status === 200) {
+          let token = res.data.token;
+          localStorage.setItem("user", token);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
     return res;
-  }
-  , getUserDetails() {
+  },
+  getUserDetails() {
     let token = localStorage.getItem("user");
     try {
       return VueJwtDecode.decode(token);
     } catch (error) {
-      console.log(error, 'error from decoding token');
+      console.log(error, "error from decoding token");
     }
-  }
-  , async getUsers() {
-    return fetch(this.auth + '/users', this.options)
-        .then(data => data.json())
-        .then(json => {return json});
-  }
-  , updateUser(data) {
+  },
+  async getUsers() {
+    return fetch(this.auth + "/users", this.options)
+      .then((data) => data.json())
+      .then((json) => {
+        return json;
+      });
+  },
+  updateUser(data) {
     axios({
-      method : 'post',
-      url : this.auth + '/update',
-      headers : {
-        'Content-Type' : 'application/json',
-        'Authorization' : localStorage.getItem("user"),
+      method: "post",
+      url: this.auth + "/update",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("user"),
       },
-      data : data
+      data: data,
     });
-  }
-  , logout() { localStorage.removeItem("user"); }
-  , isAuthenticated() {
-    return localStorage.getItem('user') && this.getUserDetails()
-  }
-  , isAdminUser() {
-    return localStorage.getItem('user') &&
-           this.getUserDetails().role === 'admin'
-  }
-}
+  },
+  logout() {
+    localStorage.removeItem("user");
+  },
+  isAuthenticated() {
+    return localStorage.getItem("user") && this.getUserDetails();
+  },
+  isAdminUser() {
+    return (
+      localStorage.getItem("user") && this.getUserDetails().role === "admin"
+    );
+  },
+};
